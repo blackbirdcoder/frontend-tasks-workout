d9fc0197b0a73eaf338953e94beba6a9bcd4961c
Eugene-TechnoNinja <technoninja@ukr.net>
2025-05-23 23:43:52 +0300

    task ready

---
+++ homework5/javascript/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const username = prompt('Enter name:');
+if (username) {
+    const answer = confirm('Do you want to see the name?');
+    if (answer) alert('Your name ' + username);
+}
+
