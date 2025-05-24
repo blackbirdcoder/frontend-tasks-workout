3f14a6c86942037760a31954bf662150010ebf43
Eugene-TechnoNinja <technoninja@ukr.net>
2025-05-24 17:04:21 +0300

    task refactoring completed

--- hw5/js/script.js
+++ hw5/js/script.js
@@ -1,8 +1,7 @@
-'use strict'
+'use strict';
 
 const username = prompt('Enter name:');
 if (username) {
     const answer = confirm('Do you want to see the name?');
     if (answer) alert('Your name ' + username);
 }
-
