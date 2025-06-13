4f0d27ede8482f2554630d6d9ce4cbe04d4513e5
Eugene-TechnoNinja <technoninja@ukr.net>
2025-06-13 09:24:59 +0300

    improved code

--- js/script.js
+++ js/script.js
@@ -1,39 +1,49 @@
+'use strict';
+
 //----- Task 1
 let line = '';
-for (let i = 20; i <= 30; i += 0.5) line += i + ' ';
+for (let i = 20; i <= 30; i += 0.5) {
+    line += `${i} `;
+}
 console.log(line);
 //-----------
 
 //----- Task 2
-const HRY = 27;
-for (let i = 10; i <= 100; i += 10) console.log(`${i}$=${i * HRY}`);
+const ONE_DOLLAR_HRYVNIA = 27;
+for (let i = 10; i <= 100; i += 10) {
+    console.log(`${i}$=${i * ONE_DOLLAR_HRYVNIA}`);
+}
 //-----------
 
 //----- Task 3
 {
-    const N = 90;
+    const GIVEN_NUMBER = 90;
     for (let i = 1; i <= 100; i++) {
         const squ = i * i;
-        if (squ < N) console.log(squ);
+        if (squ < GIVEN_NUMBER) console.log(squ);
     }
 }
 //-----------
 
 //----- Task 4
 {
-    const N = 7;
+    const GIVEN_NUMBER = 7;
     const result = [];
-    for (let i = 1; i <= 100; i++) if (N % i === 0) result.push(i);
-    result.length === 2 ? console.log(`natural number ${N}`) : console.log(`NOT natural number ${N}`);
+    for (let i = 1; i <= 100; i++) {
+        if (GIVEN_NUMBER % i === 0) result.push(i);
+    }
+    result.length === 2
+        ? console.log(`natural number ${GIVEN_NUMBER}`)
+        : console.log(`NOT natural number ${GIVEN_NUMBER}`);
 }
 //-----------
 
 //----- Task 5
 {
-    const N = 3;
+    const GIVEN_NUMBER = 3;
     const TARGET = 81;
     for (let i = 0; i <= TARGET; i++) {
-        const pow = Math.pow(N, i);
+        const pow = Math.pow(GIVEN_NUMBER, i);
         if (pow === TARGET) {
             console.log(true + ' ' + TARGET);
             break;
