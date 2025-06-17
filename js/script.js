58b974d684cc64b4f3b4829307115ec2dfc1a097
Eugene-TechnoNinja <technoninja@ukr.net>
2025-06-17 14:26:22 +0300

    ready task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function padString(line, number, char, right = true) {
+    const error = '{1} argument is not correct, contains: {2}';
+
+    if (line === undefined) {
+        return error.replace('{1}', 'String').replace('{2}', line);
+    }
+    if (number === undefined || isNaN(number)) {
+        return error.replace('{1}', 'Number').replace('{2}', number);
+    }
+    if (char === undefined || char.length !== 1) {
+        return error.replace('{1}', 'String (one sing)').replace('{2}', char);
+    }
+
+    if (number < line.length) return line.substring(0, number);
+
+    return right ? line + char.repeat(number) : char.repeat(number) + line;
+}
+
+const stringOne = padString('hello', 6, '*', false);
+console.log(stringOne);
+
+const stringTwo = padString('hello', 6, '*', true);
+console.log(stringTwo);
+
+const stringThree = padString('hello', 2, '*', true);
+console.log(stringThree);
