f7536a685cfaa449806093e8e4789209ba06adaa
Eugene-TechnoNinja <technoninja@ukr.net>
2025-06-30 19:02:53 +0300

    task ready

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const data = {
+    id: 1,
+    name: 'root',
+    meta: {
+        id: 2,
+        parent: {
+            id: 3,
+            name: 'leaf',
+        },
+    },
+    array: [{ id: 4 }, { name: 'node', children: [{ id: 5 }] }],
+};
+
+function findValuesByKey(dataSet, target, box = []) {
+    for (const item in dataSet) {
+        if (item === target) box.push(dataSet[item]);
+
+        if (typeof dataSet[item] === 'object' && dataSet[item] !== null) {
+            findValuesByKey(dataSet[item], target, box);
+        }
+    }
+
+    return box;
+}
+
+const result = findValuesByKey(data, 'id');
+console.log(result);
